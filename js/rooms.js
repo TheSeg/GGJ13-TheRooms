b5edc1d764d3bf9aa99e2aef4917024466129f88
Seg <seg@segonmedia.com>
2013-01-27 13:05:32 -0800

    Progress Check-in

--- js/rooms.js
+++ js/rooms.js
@@ -142,6 +142,59 @@ function roomEval( targetRoom ) {
             ];
             break;
             
+        case "WHITE_MIDDLE_DEFAULT":
+            color = 'white';
+            desc = WHITE_MIDDLE_DEFAULT();
+            actions = [
+                "GO WEST",
+                "GO EAST",
+                "GO SOUTH",
+                "GO NORTH",
+            ];
+            hrefs = [
+                'WHITE_WEST_000',
+                'WHITE_EAST_000',
+                'WHITE_SOUTH_000',
+                'WHITE_NORTH_000',
+            ];
+            links = [
+                null,
+            ];
+            break;
+            
+        case "WHITE_WEST_000":
+            color = 'white';
+            tempReturn = WhiteRoom_DoorApproch( "TEAL" );
+            desc = tempReturn[0];
+            actions = tempReturn[1];
+            hrefs = tempReturn[2];
+            links = tempReturn[3];
+            
+            break;
+        
+        case "TEAL_ENTRY":
+            state['TEAL_FIRSTVIST'] = true;
+            
+            color = 'teal'
+            actions = [];
+            hrefs = [];
+            links = [];
+            
+            desc = "As soon as you touch the outlined space, the light and walls change to the color of <b>TEAL</b>. The outline of the door also changes to a solid white.<br/><br/>In the middle of the room, you see a pedistal in the middle of the room. ";
+
+            if ( state['ROOM_CLEAR_TEAL'] == false ) {
+                // Room is finished
+                desc = desc + "You see a <b>TEAL</b> cube upon the pedistal. There's no reason to remove it.";
+            } else if ( state['INV_BLOCK_TEAL'] ) {
+                // Player has a TEAL block
+            }
+            
+            actions.push("TOUCH the door outline.");
+            hrefs.push("WHITE_WEST_001")
+            
+            break;
+            
+        // Default catch for when someone forgot to make the room state...
         default:
             color = null;
             desc = null;
@@ -158,4 +211,81 @@ function roomEval( targetRoom ) {
     }
     
     return [color, desc, actions, hrefs, links];
+}
+
+function WhiteRoom_DoorApproch( TargetDoorColor ) {
+    // Used to make the same generic door approch for the white room.
+    var desc;
+    var actions;
+    var hrefs;
+    var links;
+
+    // If The room is clear, show that process.
+    if ( state['ROOM_CLEAR_'+TargetDoorColor.toUpperCase()] ) {
+        // Room is cleared. Show the 'locked' state.
+        desc = "You walk to the wall to what was a "+TargetDoorColor.toUpperCase()+" color outline, but is now simply RED.<br/><br/>There is a slightly louder hum of in rhythm eminiating from the door.";
+        // Actions are the same acorss.
+        actions = [
+            "GO BACK to the middle of the room.",
+        ];
+        hrefs = [
+            "WHITE_MIDDLE_DEFAULT"
+        ];
+        links = [
+            null,
+        ];
+    } else {
+        // Room is not cleared.
+        if ( state['WHITE_ANY_DOOR_VISITED'] === false ) {
+            // Room is first visit to ANY door.
+            state['WHITE_ANY_DOOR_VISITED'] = true;
+            desc = "Cautiously walking towards the outline, you try to peak into the outline to observe what lies beyond the door.<br/><br/>When you look closely at the outline, you see a solid hue of "+TargetDoorColor.toUpperCase()+". Still, there are no objects your can recognize beyond the wall. There is also a very low hum of a sound. You can feel the sound more than you can hear it.";
+        } else {
+            // Subsquent room visits.
+            desc = "You walk to the "+TargetDoorColor.toUpperCase()+" color outline on the wall.<br/><br/>There is still a very low hum of a sound in rhythm. You can feel the sound more than you can hear it.";
+        }
+        // Actions are the same acorss.
+        actions = [
+            "TOUCH the door space.",
+            "GO BACK to the middle of the room.",
+        ];
+        hrefs = [
+            TargetDoorColor.toUpperCase()+"_ENTRY",
+            "WHITE_MIDDLE_DEFAULT"
+        ];
+        links = [
+            null,
+        ];
+    }
+    
+    // The Return
+    return [desc, actions, hrefs, links];
+}
+
+function WHITE_MIDDLE_DEFAULT() {
+    // Check and returns which doors are active.
+    desc = "You are in the center of the room with four outlines of doors around the room.<br/><br/>";
+    
+    if ( state['ROOM_CLEAR_TEAL'] ) {
+        desc = desc + "To the <b>WEST</b>, you see a <b>RED</b> outline of a door.<br/>";
+    } else {
+        desc = desc + "To the <b>WEST</b>, you see a <b>TEAL</b> outline of a door.<br/>";
+    }
+    if ( state['ROOM_CLEAR_PURPLE'] ) {
+        desc = desc + "To the <b>EAST</b>, you see a <b>RED</b> outline of a door.<br/>";
+    } else {
+        desc = desc + "To the <b>EAST</b>, you see a <b>PURPLE</b> outline of a door.<br/>";
+    }
+    if ( state['ROOM_CLEAR_OLIVE'] ) {
+        desc = desc + "To the <b>SOUTH</b>, you see a <b>RED</b> outline of a door.<br/>";
+    } else {
+        desc = desc + "To the <b>SOUTH</b>, you see a <b>OLIVE</b> outline of a door.<br/>";
+    }
+    if ( state['ROOM_CLEAR_GRAY'] ) {
+        desc = desc + "To the <b>NORTH</b>, you see a <b>RED</b> outline of a door.<br/>";
+    } else {
+        desc = desc + "To the <b>NORTH</b>, you see a <b>GRAY</b> outline of a door.<br/>";
+    }
+    
+    return desc;
 }
--- js/state_machine.js
+++ js/state_machine.js
@@ -7,10 +7,30 @@
 */
 
 var state = {
-    'WHITE_000_002_yelled':0,
     'ROOM_CLEAR_TEAL':false,
     'ROOM_CLEAR_PURPLE':false,
     'ROOM_CLEAR_OLIVE':false,
     'ROOM_CLEAR_GRAY':false,
     'ROOM_CLEAR_RED':false,
+    
+    'WHITE_000_002_yelled':0,
+    'WHITE_ANY_DOOR_VISITED':false,
+    
+    'TEAL_FIRSTVIST':false,
+    'TEAL_PEDISTAL':'PURPLE',
+    
+    'PURPLE_FIRSTVIST':false,
+    'PURPLE_PEDISTAL':'TEAL',
+    
+    'OLIVE_FIRSTVIST':false,
+    'OLIVE_PEDISTAL':'GRAY',
+    
+    'GRAY_FIRSTVIST':false,
+    'GRAY_PEDISTAL':'OLIVE',
+    
+    'INV_BLOCK_TEAL':false,
+    'INV_BLOCK_PURPLE':false,
+    'INV_BLOCK_OLIVE':false,
+    'INV_BLOCK_GRAY':false,
+    
 };
